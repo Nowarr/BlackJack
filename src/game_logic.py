3a57d71ae1b1bac799f3be344d75aeb2d4b5df07
akrm al-hakimi <alhakimiakrmj@gmail.com>
2024-08-19 13:10:50 -0400

    factored into class

--- src/game_logic.py
+++ src/game_logic.py
@@ -1,18 +1,21 @@
-import numpy as np 
 import random
+import numpy 
+
+class Game:
+    def __init__(self):
+        self.deck = {
+            "ace": (1, 11), "two": 2, "three": 3, "four": 4, "five": 5,
+            "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10,
+            "jack": 10, "queen": 10, "king": 10
+        }
+        self.suits = ["hearts", "clubs", "spades", "diamonds"]
+
+    def deal(self):
+        card, value = random.choice(list(self.deck.items()))  # random card and its value from deck
+        suit = random.choice(self.suits)  # random suit
+        return card, value, suit
 
 
-def deal():
-    deck = {"ace":(1,11),"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,
-            "eight":8,"nine":9,"ten":10,"jack":10,"queen":10,"king":10}
-    suits = ["hearts","clubs","spades","diamonds"]
-
-    card,value = random.choice(list(deck.items())) # random card and its value from deck 
-    suit = random.choice(suits) # random suit
-
-    
-    return(card,value,suit)
-
-
-
-print(deal())
+if __name__ == "__main__":
+    game = Game()
+    print(game.deal())
